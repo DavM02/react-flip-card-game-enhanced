f22a7d596137cff868e2b5d000a34ced3b0f4d32
Dav <dmatosyan2002@gmail.com>
2025-04-17 20:39:27 +0400

    upd

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -10,49 +10,56 @@ const Board: React.FC = () => {
   const { level, pairNumber } = gameStore()
   const { isStarted } = statisticStore()
 
-  const { numberOfCells, selected,
+  const {
+    numberOfCells,
+    selected,
     flipped,
     matches,
     adjustTransition,
     onCardClick,
-    setAdjustTransition, color } = useGameLogic()
+    setAdjustTransition,
+    color,
+  } = useGameLogic()
 
-  const { images, isLoading } = useImages(numberOfCells, pairNumber)
-
+  const { images, isLoading, isError, error } = useImages(numberOfCells, pairNumber)
 
   return (
     <div className={`board ${level}`}>
-      {
-        isLoading ? <h3>Preparing the board</h3> : <ul>
-          {images && images.map((el, i) => {
-            const img = el.resized_url;
-            const id = el.id
-            const matched = matches.includes(i);
-            const isFlipped = flipped.includes(i) || matched;
-            const isSelected = selected.some((sel) => sel.i === i);
+      {isError && <p>{error?.message}</p>}
 
-            return (
-              <Card
-                key={i}
-                i={i}
-                img={img || ""}
-                isStarted={isStarted}
-                isFlipped={isFlipped}
-                isMatched={matched}
-                isSelected={isSelected}
-                color={color}
-                onClick={() => {
-                  if (!adjustTransition) setAdjustTransition(true);
-                  onCardClick({ i, path: id });  
-                }}
+      {!isError && (
+        isLoading ? (
+          <h3>Preparing the board</h3>
+        ) : (
+          <ul>
+            {images && images.map((el, i) => {
+              const img = el.resized_url;
+              const id = el.id;
+              const matched = matches.includes(i);
+              const isFlipped = flipped.includes(i) || matched;
+              const isSelected = selected.some((sel) => sel.i === i);
 
-                adjustTransition={adjustTransition}
-              />
-            );
-          })}
-        </ul>
-      }
-
+              return (
+                <Card
+                  key={i}
+                  i={i}
+                  img={img || ""}
+                  isStarted={isStarted}
+                  isFlipped={isFlipped}
+                  isMatched={matched}
+                  isSelected={isSelected}
+                  color={color}
+                  onClick={() => {
+                    if (!adjustTransition) setAdjustTransition(true);
+                    onCardClick({ i, path: id });
+                  }}
+                  adjustTransition={adjustTransition}
+                />
+              );
+            })}
+          </ul>
+        )
+      )}
     </div>
   );
 };
